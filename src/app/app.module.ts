9666a95488d4bd4e2f970008c044c709d5ec25f7
Fernando Orozco <forozcomercado@gmail.com>
2024-09-10 19:36:16 -0500

    feat(UI) initial UI with left menu and main content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
